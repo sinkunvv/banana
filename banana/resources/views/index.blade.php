78fd367725ae9c1e1f1ca12fa8e62fdbb45c789c
sinkunvv <sinkunvv@gmail.com>
2017-10-01 23:44:26 +0900

    layouts

--- banana/resources/views/index.blade.php
+++ banana/resources/views/index.blade.php
@@ -15,40 +15,45 @@
             <h3>メーカー</h3>
         </header>
         <div class="row">
-            <img src={{asset('/img/test1.png')}} width="30%" height="30%" alt="" />
-            <img src={{asset('/img/test2.png')}} width="30%" height="30%" alt="" />
-            <img src={{asset('/img/test3.png')}} width="30%" height="30%" alt="" />
-        </div>
+            <div class="6u 12u(narrower)">
+                <img src={{asset('/img/test1.png')}} alt="" />
+                <img src={{asset('/img/test2.png')}} alt="" />
+                <img src={{asset('/img/test3.png')}} alt="" />
+            </div>
 
-        {!! Form::open(['files' => 'true']) !!}
-            <div class="row 50%">
-                <div class="12u">
-                    {!! Form::label('お題：(250x250pxまで)') !!}
-                    {!! Form::text('this_theme', null, ['class' => 'form-control', 'placeholder' => 'お題を入力']) !!}
-                    {!! Form::file('obj_img') !!}
-                </div>
+            <div class="6u 12u(narrower)">
+                <section>
+                    {!! Form::open(['files' => 'true']) !!}
+                        <div class="row 50%">
+                            <div class="12u">
+                                {!! Form::label('お題：(250x250pxまで)') !!}
+                                {!! Form::text('this_theme', null, ['class' => 'form-control', 'placeholder' => 'お題を入力']) !!}
+                                {!! Form::file('obj_img') !!}
+                            </div>
+                        </div>
+                        <div class="row 50%">
+                            <div class="12u">
+                                {!! Form::label('頭のいい人の場合')!!}
+                                @for ($i=1;$i<=8;$i++)
+                                    {!! Form::text('smart_think[]', null, ['class' => 'form-control', 'placeholder' => '頭のいい考えをかこう...']) !!}
+                                @endfor
+                            </div>
+                        </div>
+                        <div class="row 50%">
+                            <div class="12u">
+                                {!! Form::label('頭の悪い人の場合')!!}
+                                {!! Form::text('stupid_think', null, ['class' => 'form-control', 'placeholder' => 'ばばな']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="12u">
+                                <ul class="buttons">
+                                    <li>{!! Form::submit('じっこう', ['class' => 'special']) !!}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    {!! Form::close() !!}
+                </section>
             </div>
-            <div class="row 50%">
-                <div class="12u">
-                    {!! Form::label('頭のいい人の場合')!!}
-                    @for ($i=1;$i<=8;$i++)
-                        {!! Form::text('smart_think[]', null, ['class' => 'form-control', 'placeholder' => '頭のいい考えをかこう...']) !!}
-                    @endfor
-                </div>
-            </div>
-            <div class="row 50%">
-                <div class="12u">
-                    {!! Form::label('頭の悪い人の場合')!!}
-                    {!! Form::text('stupid_think', null, ['class' => 'form-control', 'placeholder' => 'ばばな']) !!}
-                </div>
-            </div>
-            <div class="row">
-                <div class="12u">
-                    <ul class="buttons">
-                        <li>{!! Form::submit('じっこう', ['class' => 'special']) !!}</li>
-                    </ul>
-                </div>
-            </div>
-        {!! Form::close() !!}
      </section>
 @endsection
