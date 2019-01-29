d24cdc50939cf9fae047c1968ec35988ce851634
sinkunvv <sinkunvv@gmail.com>
2019-01-29 17:23:09 +0900

    fix

--- banana/resources/views/index.blade.php
+++ banana/resources/views/index.blade.php
@@ -1,65 +1,65 @@
 @extends('layouts.master')
 
 @section('content')
-    <section>
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+<section>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <div class="content">
+        <section>
+            <div class='sample_img'>
+                <img src={{asset('/img/theme.png')}} alt="" class='sample_img' />
+                <img src={{asset('/img/smart.png')}} alt="" class='sample_img' />
+                <img src={{asset('/img/stupid.png')}} alt="" class='sample_img' />
             </div>
-        @endif
-        <div class="content">
-            <section>
-                <div class='sample_img'>
-                    <img src={{asset('/img/theme.png')}} alt="" class='sample_img'/>
-                    <img src={{asset('/img/smart.png')}} alt="" class='sample_img'/>
-                    <img src={{asset('/img/stupid.png')}} alt="" class='sample_img'/>
+            {!! Form::open(['files' => 'true', 'url'=>'generate']) !!}
+            {{Form::token()}}
+            <div class="row 50%">
+                <div class="12u">
+                    {!! Form::label('お題：(500x500pxまで)') !!}
+                    {!! Form::text('this_theme', null, ['class' => 'form-control', 'placeholder' => '例えば○○を見た時']) !!}
+                    {!! Form::file('obj_img') !!}
                 </div>
-                {!! Form::open(['files' => 'true', 'url'=>'generate']) !!}
-                    <div class="row 50%">
-                        <div class="12u">
-                            {!! Form::label('お題：(500x500pxまで)') !!}
-                            {!! Form::text('this_theme', null, ['class' => 'form-control', 'placeholder' => '例えば○○を見た時']) !!}
-                            {!! Form::file('obj_img') !!}
-                        </div>
-                    </div>
-                    <div class="row 50%">
-                        <div class="12u">
-                            {!! Form::label('頭のいい人の場合')!!}
-                            @for ($i=1;$i<=8;$i++)
-                                @if ($i<=5)
-                                    {!! Form::text('smart_think[]', null, ['class' => 'form-control', 'placeholder' => '必須入力です..']) !!}
-                                @else
-                                    {!! Form::text('smart_think[]', null, ['class' => 'form-control']) !!}
-                                @endif
-                            @endfor
-                        </div>
-                    </div>
-                    <div class="row 50%">
-                        <div class="12u">
-                            {!! Form::label('頭の悪い人の場合')!!}
-                            {!! Form::text('stupid_think', null, ['class' => 'form-control', 'placeholder' => 'ばばな']) !!}
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="12u">
-                            <ul class="buttons">
+            </div>
+            <div class="row 50%">
+                <div class="12u">
+                    {!! Form::label('頭のいい人の場合')!!}
+                    @for ($i=1;$i<=8;$i++) @if ($i<=5) {!! Form::text('smart_think[]', null, ['class'=> 'form-control',
+                        'placeholder' => '必須入力です..']) !!}
+                        @else
+                        {!! Form::text('smart_think[]', null, ['class' => 'form-control']) !!}
+                        @endif
+                        @endfor
+                </div>
+            </div>
+            <div class="row 50%">
+                <div class="12u">
+                    {!! Form::label('頭の悪い人の場合')!!}
+                    {!! Form::text('stupid_think', null, ['class' => 'form-control', 'placeholder' => 'ばばな']) !!}
+                </div>
+            </div>
+            <div class="row">
+                <div class="12u">
+                    <ul class="buttons">
 
-                                <li>{!! Form::submit('じっこう', ['id' => 'submit-btn', 'class' => 'special']) !!}</li>
-                            </ul>
-                        </div>
-                    </div>
-                {!! Form::close() !!}
-                <div class="adsence">
-                    <h1>スポンサーリンク</h1>
-                    <!-- admax -->
-                    <script src="//adm.shinobi.jp/s/32ea2b2625e8b6610899ba1c46156e9d"></script>
-                    <!-- admax -->
+                        <li>{!! Form::submit('じっこう', ['id' => 'submit-btn', 'class' => 'special']) !!}</li>
+                    </ul>
                 </div>
-            </section>
-        </div>
-     </section>
+            </div>
+            {!! Form::close() !!}
+            <div class="adsence">
+                <h1>スポンサーリンク</h1>
+                <!-- admax -->
+                <script src="//adm.shinobi.jp/s/32ea2b2625e8b6610899ba1c46156e9d"></script>
+                <!-- admax -->
+            </div>
+        </section>
+    </div>
+</section>
 @endsection
--- banana/resources/views/layouts/master.blade.php
+++ banana/resources/views/layouts/master.blade.php
@@ -1,86 +1,86 @@
-    <!DOCTYPE HTML>
-    <!--
+<!DOCTYPE HTML>
+<!--
     Twenty by HTML5 UP
     html5up.net | @ajlkn
     Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
     -->
-    <html>
-    <head>
-        <title>ばななメーカー</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <!--[if lte IE 8]><script src={{asset('/js/ie/html5shiv.js')}}></script><![endif]-->
-        <link rel="stylesheet" href={{asset('/css/main.css')}} />
-        <!--[if lte IE 8]><link rel="stylesheet" href={{asset('/css/ie8.css')}} /><![endif]-->
-        <!--[if lte IE 9]><link rel="stylesheet" href={{asset('/css/ie9.css')}} /><![endif]-->
-        {{ Html::favicon(asset('/img/icon.png'))}}
-        <!-- Global Site Tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107705564-1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'UA-107705564-1');
-        </script>
-    </head>
-    <body class="no-sidebar">
-        <div id="page-wrapper">
+<html>
 
-            <!-- Header -->
-                <header id="header">
-                    <h1 id="logo"><a href="/">Banana <span>Maker</span></a></h1>
-                </header>
+<head>
+    <title>ばななメーカー</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src={{asset('/js/ie/html5shiv.js')}}></script><![endif]-->
+    <link rel="stylesheet" href={{asset('/css/main.css')}} />
+    <!--[if lte IE 8]><link rel="stylesheet" href={{asset('/css/ie8.css')}} /><![endif]-->
+    <!--[if lte IE 9]><link rel="stylesheet" href={{asset('/css/ie9.css')}} /><![endif]-->
+    {{ Html::favicon(asset('/img/icon.png'))}}
+    <!-- Global Site Tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107705564-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-            <!-- Main -->
-                <article id="main">
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-107705564-1');
+    </script>
+</head>
 
-                    <header class="container">
-                        <a class="twitter-timeline" width="100%" height="100px" href="https://twitter.com/hashtag/%E3%81%B0%E3%81%AA%E3%81%AA%E3%83%A1%E3%83%BC%E3%82%AB%E3%83%BC" data-widget-id="916921309224824832">#ばななメーカー のツイート</a>
-                        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-                    </header>
+<body class="no-sidebar">
+    <div id="page-wrapper">
 
-                    <!-- One -->
-                        <section class="wrapper style4 container">
-                            <!-- Content -->
-                                <div class="content">
-                                    @yield('content')
-                                </div>
+        <!-- Header -->
+        <header id="header">
+            <h1 id="logo"><a href="/">Banana <span>Maker</span></a></h1>
+        </header>
 
-                        </section>
+        <!-- Main -->
+        <article id="main">
+            <!-- One -->
+            <section class="wrapper style4 container">
+                <!-- Content -->
+                <div class="content">
+                    @yield('content')
+                </div>
 
-                </article>
+            </section>
 
-            <!-- Footer -->
-                <footer id="footer">
-                    {{-- <ul class="icons">
-                        <li><a href="#" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>
-                        <li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
-                        <li><a href="#" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>
-                        <li><a href="#" class="icon circle fa-github"><span class="label">Github</span></a></li>
-                        <li><a href="#" class="icon circle fa-dribbble"><span class="label">Dribbble</span></a></li>
-                    </ul> --}}
+        </article>
 
-                    <ul class="copyright">
-                        <li>&copy; S.DEVELOP</li>
-                        <li><a href='/agreement' target='_blank'>利用規約</a></li>
-                        <li>Developer : <a href="https://twitter.com/sinkunvv">@@sinkunvv</a></li>
-                        <li>iori-fonts : <a href="http://susu.cc/">煤式自動連結器</a></li>
-                        <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                    </ul>
+        <!-- Footer -->
+        <footer id="footer">
+            {{-- <ul class="icons">
+                <li><a href="#" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>
+                <li><a href="#" class="icon circle fa-github"><span class="label">Github</span></a></li>
+                <li><a href="#" class="icon circle fa-dribbble"><span class="label">Dribbble</span></a></li>
+            </ul> --}}
 
-                </footer>
+            <ul class="copyright">
+                <li>&copy; S.DEVELOP</li>
+                <li><a href='/agreement' target='_blank'>利用規約</a></li>
+                <li>Developer : <a href="https://twitter.com/sinkunvv">@@sinkunvv</a></li>
+                <li>iori-fonts : <a href="http://susu.cc/">煤式自動連結器</a></li>
+                <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            </ul>
 
-        </div>
+        </footer>
 
-        <!-- Scripts -->
-            <script src={{asset('/js/jquery.min.js')}}></script>
-            <script src={{asset('/js/jquery.dropotron.min.js')}}></script>
-            <script src={{asset('/js/jquery.scrolly.min.js')}}></script>
-            <script src={{asset('/js/jquery.scrollgress.min.js')}}></script>
-            <script src={{asset('/js/skel.min.js')}}></script>
-            <script src={{asset('/js/util.js')}}></script>
-            <!--[if lte IE 8]><script src={{asset('/js/ie/respond.min.js')}}></script><![endif]-->
-            <script src={{asset('/js/main.js')}}></script>
+    </div>
 
-    </body>
-    </html>
+    <!-- Scripts -->
+    <script src={{asset('/js/jquery.min.js')}}></script>
+    <script src={{asset('/js/jquery.dropotron.min.js')}}></script>
+    <script src={{asset('/js/jquery.scrolly.min.js')}}></script>
+    <script src={{asset('/js/jquery.scrollgress.min.js')}}></script>
+    <script src={{asset('/js/skel.min.js')}}></script>
+    <script src={{asset('/js/util.js')}}></script>
+    <!--[if lte IE 8]><script src={{asset('/js/ie/respond.min.js')}}></script><![endif]-->
+    <script src={{asset('/js/main.js')}}></script>
+
+</body>
+
+</html>
